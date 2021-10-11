3ce934a889a001ade44728b9613ee0da1e3ea994
Emre <61523720+yilmazemre61@users.noreply.github.com>
2021-10-11 16:38:53 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        PerfectNumber.isPerfectNumber(6);
+        PerfectNumber.isPerfectNumber(9);
+        PerfectNumber.isPerfectNumber(28);
+        PerfectNumber.isPerfectNumber(5);
+        PerfectNumber.isPerfectNumber(-1);
+    }
+}
